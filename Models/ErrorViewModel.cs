359a7dccc026858b7017609be55c9eeea89e4c55
IGNACIO LEÓN PEREZ <49961152@ort>
2026-05-05 07:51:59 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Tp_Formularios.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
